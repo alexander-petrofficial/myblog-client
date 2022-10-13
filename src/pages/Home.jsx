964cecdbf1ecf1404f784cb71d5aa5467b9595a8
Petrov Alexander <petrov.aleksandr@phystech.edu>
2022-10-13 03:27:29 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Grid from "@mui/material/Grid";
+
+import axios from "../axios";
+
+import { Post } from "../components/Post";
+import { TagsBlock } from "../components/TagsBlock";
+import { CommentsBlock } from "../components/CommentsBlock";
+import { fetchPosts, fetchTags } from "../redux/slices/posts";
+import { useSearchParams } from "react-router-dom";
+
+export const Home = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth.data);
+  const { posts, tags } = useSelector((state) => state.posts);
+
+  const [sortedBy, setSortedBy] = React.useState(0);
+
+  const isPostsLoading = posts.status === "loading";
+  const isTagsLoading = tags.status === "loading";
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  React.useEffect(() => {
+    dispatch(fetchPosts(sortedBy));
+    dispatch(fetchTags());
+  }, [sortedBy]);
+
+  return (
+    <>
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={sortedBy}
+        aria-label="basic tabs example"
+      >
+        <Tab
+          onClick={() => {
+            setSortedBy(0);
+            setSearchParams({ sortedBy });
+          }}
+          label="Новые"
+        />
+        <Tab
+          onClick={() => {
+            setSortedBy(1);
+            setSearchParams({ sortedBy });
+          }}
+          label="Популярные"
+        />
+      </Tabs>
+      <Grid container spacing={4}>
+        <Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading={true} />
+            ) : (
+              <Post
+                key={obj._id}
+                _id={obj._id}
+                title={obj.title}
+                imageUrl={
+                  obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
+                }
+                user={obj.user}
+                createdAt={obj.createdAt}
+                viewsCount={obj.viewsCount}
+                commentsCount={3}
+                tags={obj.tags}
+                isEditable={userData?._id === obj.user._id}
+              />
+            )
+          )}
+        </Grid>
+        <Grid xs={4} item>
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
+          <CommentsBlock
+            items={[
+              {
+                user: {
+                  fullName: "Вася Пупкин",
+                  avatarUrl: "https://mui.com/static/images/avatar/1.jpg",
+                },
+                text: "Это тестовый комментарий",
+              },
+              {
+                user: {
+                  fullName: "Иван Иванов",
+                  avatarUrl: "https://mui.com/static/images/avatar/2.jpg",
+                },
+                text: "When displaying three lines or more, the avatar is not aligned at the top. You should set the prop to align the avatar at the top",
+              },
+            ]}
+            isLoading={false}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
